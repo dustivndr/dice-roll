1d4cff68cf7f8b87c262d4157983f9e333f67aa5
abduznik <85239936+abduznik@users.noreply.github.com>
2026-01-03 13:07:31 +0200

    feat(pascal): implement menu navigation and arrow key handling

--- Pascal/dice.pas
+++ Pascal/dice.pas
@@ -1,23 +1,60 @@
 program DiceRoll;
 
-uses crt; { Standard unit for terminal control }
+uses crt; { Standart unit for terminal control }
 
-{ Basic procedure to draw the dice based on view.txt template }
+var
+  key: Char;
+  isRollingSelected: Boolean;
+  diceValue: Integer;
+
 procedure ShowDice(val: Integer);
 begin
-    Writeln('    +---+');
-    Writeln('    | ', val, ' |');
-    Writeln('    +---+');
-    Writeln;
+  Writeln('    +---+');
+  Writeln('    | ', val, ' |');
+  Writeln('    +---+');
+  Writeln;
+end;
+
+procedure DisplayMenu(val: Integer; rollSelected: Boolean);
+begin
+  ClrScr;
+  ShowDice(val);
+  if rollSelected then
+  begin
+    Writeln(' > Roll the dice');
+    Writeln('   Exit');
+  end
+  
+  else
+  begin
+    Writeln('   Roll the dice');
+    Writeln(' > Exit');
+  end;
 end;
 
 begin
-    ClrScr; { Clear screen }
-    ShowDice(1);
-    Writeln(' > Roll the dice');
-    Writeln('   Exit');
+  diceValue := 1;
+  isRollingSelected := True;
+  
+  repeat
+    DisplayMenu(diceValue, isRollingSelected);
     
-    Writeln;
-    Write('Press any key to exit setup test...');
-    ReadKey;
+    key := ReadKey;
+    if key = #0 then { Arrow Keys }
+    begin
+      key := ReadKey;
+      case key of
+        #72, #80: isRollingSelected := not isRollingSelected; { Up & Down }
+      end;
+    end
+    
+    else if (key = 'w') or (key = 's') or (key = 'W') or (key = 'S') then
+    begin
+        isRollingSelected := not isRollingSelected;
+    end;
+  
+  until (key = #13) and (not isRollingSelected);
+  
+  ClrScr;
+
 end.
